aded55d89d834ed888e8e1dc1ce2ea3c4169fa4e
keidii <keidii@gmail.com>
2018-05-08 09:11:24 +0000

    refactor

--- pprint_sgf.py
+++ pprint_sgf.py
@@ -3,6 +3,7 @@ import sys
 import os
 import tempfile
 import subprocess
+import yaml
 
 KEYWORD = "segfault"
 ERRORS = {
@@ -15,39 +16,47 @@ DISASM_CACHE={}
 
 CTX_LINES = 3
 
+
+
+
 class EE(Exception):
   pass
 
-def try_parse_binary(segfault, path_to_binary):
+class HexInt(int): pass
+
+def try_parse_binary(info, path_to_binary):
   global DISASM_CACHE
+  DISASM_SUFIX="-disasm"
   import elftools.elf.elffile
-  lib_name = segfault['tgt']['lib']
+  ext = {}
+  lib_name = info['fail']['binary']
   full_fn = os.path.join(path_to_binary, lib_name)
   if not os.path.isfile(full_fn):
     raise Exception('{0} - file not found '.format(full_fn))
   if full_fn not in DISASM_CACHE:
-    tmp = tempfile.mkstemp('-disasm')[1]
-    os.system('objdump -M intel -dSlr {inp} > {out}'.format(inp=full_fn, out=tmp))
+    tmp = tempfile.mkstemp(DISASM_SUFIX)[1]
+    objdump_cmd = 'objdump -M intel -dlr {inp} > {out}'.format(inp=full_fn, out=tmp)
+    os.system(objdump_cmd)
     DISASM_CACHE[full_fn] = tmp
-  ext = {}
+    ext['_objdump'] = objdump_cmd
   ext['file_path'] = full_fn
+  ext['_tmp_path'] = DISASM_CACHE[full_fn]
   ext['disasm_line'] = ''
   ext['disasm_context'] = '' 
-  search_for = '{0:x}:'.format(segfault['tgt']['offset'])
+  look_for = '{0:x}:\t'.format(info["fail"]["binary_offset"])
+  ext['_loog_for'] = look_for
   grep_args = ['grep']
   grep_args.append('-C{0}'.format(CTX_LINES))
   grep_args.append('-e')
-  grep_args.append("\s{0}\s".format(search_for))
+  grep_args.append(look_for)
   grep_args.append(DISASM_CACHE[full_fn])
   try:
     lines = subprocess.check_output(grep_args).strip("\n").split("\n")
+    #print(lines)
     ext['disasm_context'] = lines
-    for l in lines:
-      if search_for in l:
-        ext['disasm_line'] = l
-        break
+    ext['disasm_line'] = lines[CTX_LINES]
   except Exception as err:
-    pass
+    ext['_grep_fail'] = str(err)
   return ext
 
 def unstr(hexstr):
@@ -57,47 +66,48 @@ def unstr(hexstr):
   hexstr = hexstr.rjust(just, '0')
   return ''.join(c if ord(c) >= 0x20 and ord(c) < 126 else '.' for c in hexstr.decode('hex')[::-1])
 
+# deadbeef ip 0000000000400558 sp 00007ffce3073ee0 error 4 in segf.o[400000+1000]
+FIELDS = [ "process", "__seg", "__at", "mem_addr","__ip","reg_ip","__sp","reg_sp","__er","error_code","__in","fail_org" ]
 
-def parse_segfault_line(line, path_to_binary=None):
+def parse_segfault_line(line):
   if CHECK_SEGFAULT_AT and " segfault at " not in line:
-    return None
-  try:
-    elements = line.strip().split(" ")
-    pos = elements.index(KEYWORD)
-    INFO = {}
-    INFO['proc_name'],INFO['proc_pid'] = elements[pos-1][:-2].split("[")
-    INFO['mem_addr'] = int(elements[pos+2],16)
-    INFO['reg_ip'] = int(elements[pos+4],16)
-    INFO['reg_sp'] = int(elements[pos+6],16)
-    INFO['mem_addr_str'] = unstr(elements[pos+2])
-    INFO['reg_ip_str'] = unstr(elements[pos+4])
-    INFO['reg_sp_str'] = unstr(elements[pos+6])
-    INFO['err_code'] = int(elements[pos+8])
-    INFO['err_str'] = ERRORS.get(INFO['err_code']," ?? ")
-    fail_at = elements[pos+10]
-    fail_file, fail_rva = fail_at[0:-1].split("[")
-    INFO['tgt'] = {}
-    INFO['tgt']['str'] = fail_at
-    INFO['tgt']['lib'] = fail_file
-    INFO['tgt']['mem_base'], INFO['tgt']['mem_size'] = [int(x,16) for x in fail_rva.split("+")]
-    INFO['tgt']['mem_end'] = INFO['tgt']['mem_base'] +  INFO['tgt']['mem_size']
-    if INFO['reg_ip'] > INFO['tgt']['mem_base'] and INFO['reg_ip'] < INFO['tgt']['mem_end']:
-      INFO['tgt']['offset'] = INFO['reg_ip'] - INFO['tgt']['mem_base']
-    else:
-      INFO['tgt']['offset'] = 0
-    for key in ['mem_base','mem_end','mem_size','offset']:
-      k2 = key + "_hex"
-      INFO['tgt'][k2] = "0x{0:08X}".format(INFO['tgt'][key])
-    INFO['binary'] = None
-    if path_to_binary is not None:
-      try:
-        INFO['binary'] = try_parse_binary(INFO, path_to_binary)
-      except Exception as err:
-        print("Fail to parse binary, reason: {0}".format(err))
-    return INFO
-  except Exception as err:
-    print("FAIL TO PARSE LINE, reason: {0}".format(err))
-    return None
+    raise EXception("no 'segfault at' in line !")
+  #line = line.split("segfault at",1)[1] # cut useless stuff
+
+  elements = line.strip().split(" ")
+  keyword_pos = elements.index(KEYWORD)
+  elements = elements[keyword_pos-1:]
+  info = dict(zip(FIELDS, elements))
+  for key in info.keys():
+    if key.startswith("__"):
+      del info[key]
+
+  info['process_name'], info['process_pid'] = info['process'][:-2].split("[",1)
+
+  for key in ['mem_addr', 'reg_ip', 'reg_sp']:
+    info['{0}_str'.format(key)] = unstr(info[key])
+    info[key] = HexInt(info[key], 16)
+  
+  info['error_str'] = ERRORS.get(info['error_code'], "<unknown code>")
+  fail_file, fail_rva = info['fail_org'][:-1].split("[")
+  fail_base, fail_size = [HexInt(x,16) for x in fail_rva.split("+",1)]
+  
+  eip = info['reg_ip']
+  fail_offset = 0
+  if eip > fail_base and eip < fail_base + fail_size:
+    fail_offset = eip - fail_base
+    
+  info['fail'] = dict(
+    binary = fail_file,
+    mem_base = fail_base,
+    mem_size = fail_size,
+    mem_end = HexInt(fail_base + fail_size),
+    binary_offset = HexInt(fail_offset),
+  )
+  # print(pprint_yaml(info))
+  return info
+
+
 
 def pprint_multi(info): ## TODO : wtf on 64 bit ;P
   bin_info = ''  
@@ -114,26 +124,43 @@ def pprint_multi(info): ## TODO : wtf on 64 bit ;P
 
   print("""
 >> BEGIN
-  PROC  : {proc_name}
-  PID   : {proc_pid}
+  PROC  : {process_name}
+  PID   : {process_pid}
   MEM @ : 0x{mem_addr:016x}  [{mem_addr_str}]
   EIP   : 0x{reg_ip:016x}  [{reg_ip_str}]
   ESP   : 0x{reg_sp:016x}  [{reg_sp_str}]
-  ECODE : {err_code} ({err_str})
-  CRASH-LIB: {tgt[lib]}
-  CRASH-MEM: 0x{tgt[mem_base]:016x} ... 0x{tgt[mem_end]:016x} (size 0x{tgt[mem_size]:016x})
-  EIP-BASE : 0x{tgt[offset]:016x}  == EIP - BASE
+  ECODE : {error_code} ({error_str})
+  CRASH-LIB: {fail[binary]}
+  CRASH-MEM: 0x{fail[mem_base]:016x} ... 0x{fail[mem_end]:016x} (size 0x{fail[mem_size]:016x})
+  EIP-BASE : 0x{fail[binary_offset]:016x}  == EIP - BASE
 {bin_info}
 << SEGFAULT
   """.format(bin_info=bin_info, **info))
 
 
 def pprint_single(info):
-  bin_info=''
+  tpl = [
+    "PROC=[{process_name}]",
+    "PID={process_pid}",
+    "MEM=0x{mem_addr:08x}",
+    "EIP=0x{reg_ip:08x}",
+    "ESP=0x{reg_sp:08x}",
+    "ERR={error_code}",
+    "LIB=[{fail[binary]}]",
+    "MEM-BLOCK=0x{fail[mem_base]:08x}...0x{fail[mem_end]:08x}",
+    "ADDR=0x{fail[binary_offset]:08x}",
+  ]
   if info['binary'] is not None:
-    bin_info = 'FILE={binary[file_path]}; LINE={binary[disasm_line]}'.format(**info)
-  print("PROC=[{proc_name}] PID={proc_pid} MEM=0x{mem_addr:08x} EIP=0x{reg_ip:08x} ESP=0x{reg_sp:08x} ERR={err_code} LIB=[{tgt[lib]}] MEM-BLOCK=0x{tgt[mem_base]:08x}...0x{tgt[mem_end]:08x} ADDR=0x{tgt[offset]:08x} BIN_INFO=[{bin_info}]".format(bin_info=bin_info, **info))
+    tpl.append('BIN_FILE={binary[file_path]}')
+    tpl.append('BIN_LINE=[[{binary[disasm_line]}]]')
 
+  print(" ".join(tpl).format(**info))
+
+def pprint_yaml(info):  
+  def _hexint_str(dumper, data):
+    return yaml.ScalarNode('tag:yaml.org,2002:int', "0x{0:016X}".format(data))
+  yaml.add_representer(HexInt, _hexint_str)
+  print(yaml.dump(info,default_flow_style=False))
 
 def pprint_json(info):  
   import json
@@ -143,6 +170,7 @@ FORMATTERS = {
   "json" : pprint_json,
   "single" : pprint_single,
   "multi" : pprint_multi,
+  "yaml"  : pprint_yaml,
 }
 
 FORMATTER_DEFAULT = "multi"
@@ -157,14 +185,20 @@ def main():
 
   fmt_opts = ",".join(FORMATTERS.keys())
   parser = argparse.ArgumentParser(description='Segfault Parser')
-  parser.add_argument('--logfile',action="store",  help="Path to log (dmesg?) file", required=True)
+  parser.add_argument('--logfile',action="store",  help="Path to log (dmesg?) file. '-' for STDIN.", required=True)
   parser.add_argument('--format', action="store",  help="Output format, can be [{0}], default = {1}".format(fmt_opts, FORMATTER_DEFAULT), default=FORMATTER_DEFAULT)
   parser.add_argument('--grep',   action="append", help="Look for lines containing this string. Can use multiple times.")
   parser.add_argument('--bins',   action="store",  help="Path to directory containing binaries && libs. Default=None. If specified, script will try to find binary that caused fault (by name) and parse it", default=None)
   parser.add_argument('--count',  action="store",  help="Exit after X parsed entries (like head ;)", default=-1, type=int)
   parsed_args = parser.parse_args()
 
-  file_handle = open(parsed_args.logfile,'r')
+  file_handle = None
+  if parsed_args.logfile == '-':
+    file_handle = sys.stdin
+  else:  
+    file_handle = open(parsed_args.logfile,'r')
+
+
   func = FORMATTERS.get(parsed_args.format, None)
   if func is None or not callable(func):
     print("Invalid argument: output format")
@@ -176,7 +210,21 @@ def main():
       # this way you don't need to pipe-grep-pipe
       if not is_matching_line(line, parsed_args.grep):
         continue
-    info = parse_segfault_line(line, parsed_args.bins)
+
+    info = None
+    try:
+      info = parse_segfault_line(line)
+    except:
+      pritn("<parsing failed?>")
+      continue
+    info['binary'] = None  
+    if  parsed_args.bins:
+      
+      try:
+        info['binary'] = try_parse_binary(info, parsed_args.bins)
+      except:
+        pass
+
     if info is not None:
       func(info)
     count += 1
